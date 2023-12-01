dbb319e828d79d913f4e5714892eec75cc5248c1
TARishabh <127947846+TARishabh@users.noreply.github.com>
2023-12-01 22:23:37 +0530

    markAttendance UI completed with responsive

--- src/components/MarkAttendance.jsx
+++ src/components/MarkAttendance.jsx
@@ -168,45 +168,65 @@ export default function MarkAttendance(props) {
 
     return (
         <div className="mark-attendance-container">
-            <h2 className="mark-attendance-heading">Mark Attendance</h2>
+            <h2 className="mark-attendance-heading">MARK ATTENDANCE</h2>
             <form className="mark-attendance-form">
-                <div className="form-group">
-                    <label className="label" htmlFor="subject">
-                        Select Subject:
-                    </label>
-                    <select
-                        className="select-field"
-                        id="subject"
-                        name="subject"
-                        value={selectedSubject}
-                        onChange={(e) => setSelectedSubject(e.target.value)}
-                    >
-                        <option value="" disabled>
-                            Select a subject
-                        </option>
-                        {subjects.length > 0 &&
-                            subjects.map((subject) => (
-                                <option key={subject._id} value={subject._id}>
-                                    {subject.name}
-                                </option>
-                            ))}
-                    </select>
-                </div>
+                <div className="d-flex">
+                    {/* Subject Field */}
+                    <div className="form-group flex-grow-1">
+                        <label className="label my-2" htmlFor="subject">
+                            Select Subject:
+                        </label>
+                        <select
+                            className="select-field"
+                            id="subject"
+                            name="subject"
+                            value={selectedSubject}
+                            onChange={(e) => setSelectedSubject(e.target.value)}
+                        >
+                            <option value="" disabled>
+                                Select a subject
+                            </option>
+                            {subjects.length > 0 &&
+                                subjects.map((subject) => (
+                                    <option key={subject._id} value={subject._id}>
+                                        {subject.name}
+                                    </option>
+                                ))}
+                        </select>
+                    </div>
 
-                <div className="form-group">
-                    <label className="label" htmlFor="date">
-                        Select Date:
-                    </label>
-                    <input
-                        className="input-field"
-                        type="date"
-                        id="date"
-                        name="date"
-                        value={selectedDate.toISOString().split('T')[0]}
-                        onChange={(e) => setSelectedDate(new Date(e.target.value))}
-                    />
+                    {/* Date Field */}
+                    <div className="form-group flex-grow-1">
+                        <label className="label my-2" htmlFor="date">
+                            Select Date:
+                        </label>
+                        <input
+                            className="input-field"
+                            type="date"
+                            id="date"
+                            name="date"
+                            value={selectedDate.toISOString().split('T')[0]}
+                            onChange={(e) => setSelectedDate(new Date(e.target.value))}
+                        />
+                    </div>
                 </div>
             </form>
+            <div className='d-flex'>
+                <div className="mark-all-checkbox my-1">
+                    <input
+                        style={{ border: '2px solid' }}
+                        type="checkbox"
+                        className="form-check-input"
+                        id="selectAllCheckbox"
+                        checked={selectAll}
+                        onChange={handleSelectAllChange}
+                    />
+                    <label className="form-check-label mx-3 " htmlFor="selectAllCheckbox">
+                        <strong>Mark All Present</strong>
+                    </label>
+                </div>
+            </div>
+
             <div className="container custom-margin-user-container d-flex flex-wrap">
                 {showStudents &&
                     users
@@ -216,7 +236,9 @@ export default function MarkAttendance(props) {
                                 <button
                                     type="button"
                                     className="btn card-button"
-                                    onClick={() => handleCardButtonClick(user._id)}
+                                    onClick={() => onclickUser(user._id)}
+                                    onDoubleClick={()=> onDoubleClickUser(user._id)}
+                                    style={{color: checkIfUserSelected(user._id) ? 'white' : 'black', backgroundColor: checkIfUserSelected(user._id) ? '#c4779d' : '#D9D9D9', padding: 0, border: 'none' }}
                                 >
                                     <div className="card-body">
                                         <h5 className="card-title">{user.enrollment_number.slice(10, 12)}</h5>
@@ -224,6 +246,32 @@ export default function MarkAttendance(props) {
                                 </button>
                             </div>
                         ))}
+            </div>
+            <div className="d-flex justify-content-center">
+                <button onClick={markAttendanceOfStudents} type="button" className="btn mark-button mt-4">Mark</button>
+            </div>
+            <div className="modal fade" id="userDetailsModal" tabIndex="-1" aria-labelledby="userDetailsModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 style={{fontWeight:'bold'}} className="modal-title" id="userDetailsModalLabel">User Details</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            {/* Display user details here */}
+                            {selectedUserDetails && (
+                                <div>
+                                    <p><span style={{fontWeight:'bold'}}>Name: </span> <strong>{`${selectedUserDetails.first_name} ${selectedUserDetails.last_name}`}</strong></p>
+                                    <p><span style={{fontWeight:'bold'}}>Email:</span> <strong>{selectedUserDetails.email}</strong></p>
+                                    <p><span style={{fontWeight:'bold'}}>Enrollment Number:</span> <strong>{selectedUserDetails.enrollment_number}</strong></p>
+                                </div>
+                            )}
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
@@ -260,20 +308,20 @@ export default function MarkAttendance(props) {
 //                     <input className='my-2 form-control' style={{ width: "15rem" }} type='date' id='date' name='date' value={selectedDate.toISOString().split('T')[0]} onChange={(e) => setSelectedDate(new Date(e.target.value))}></input>
 //                 </div>
 //             </div>
-//             <div className="d-flex justify-content-between align-items-center my-3">
-//                 <div className="form-check">
-//                     <input style={{ border: '1px solid' }}
-//                         type="checkbox"
-//                         className="form-check-input"
-//                         id="selectAllCheckbox"
-//                         checked={selectAll}
-//                         onChange={handleSelectAllChange}
-//                     />
-//                     <label className="form-check-label mx-2" htmlFor="selectAllCheckbox">
-//                         <strong>Mark All Present</strong>
-//                     </label>
-//                 </div>
-//             </div>
+// <div className="d-flex justify-content-between align-items-center my-3">
+//     <div className="form-check">
+//         <input style={{ border: '1px solid' }}
+//             type="checkbox"
+//             className="form-check-input"
+//             id="selectAllCheckbox"
+//             checked={selectAll}
+//             onChange={handleSelectAllChange}
+//         />
+//         <label className="form-check-label mx-2" htmlFor="selectAllCheckbox">
+//             <strong>Mark All Present</strong>
+//         </label>
+//     </div>
+// </div>
 //             <div className="container custom-margin-user-container d-flex flex-wrap">
 //                 {showStudents &&
 //                     users.sort((a, b) => parseInt(a.enrollment_number.slice(10, 12)) - parseInt(b.enrollment_number.slice(11, 13)))
@@ -294,29 +342,29 @@ export default function MarkAttendance(props) {
 //                         ))
 //                 }
 //             </div>
-//             <div className="modal fade" id="userDetailsModal" tabIndex="-1" aria-labelledby="userDetailsModalLabel" aria-hidden="true">
-//                 <div className="modal-dialog">
-//                     <div className="modal-content">
-//                         <div className="modal-header">
-//                             <h5 className="modal-title" id="userDetailsModalLabel">User Details</h5>
-//                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-//                         </div>
-//                         <div className="modal-body">
-//                             {/* Display user details here */}
-//                             {selectedUserDetails && (
-//                                 <div>
-//                                     <p style={{fontWeight:'bolder',fontSize:'1.2rem'}}><span style={{fontWeight:'bold'}}>Name: </span>{`${selectedUserDetails.first_name} ${selectedUserDetails.last_name}`}</p>
-//                                     <p style={{fontWeight:'bolder',fontSize:'1.2rem'}}><span style={{fontWeight:'bold'}}>Email:</span> {selectedUserDetails.email}</p>
-//                                     <p style={{fontWeight:'bolder',fontSize:'1.2rem'}}><span style={{fontWeight:'bold'}}>Enrollment Number:</span> {selectedUserDetails.enrollment_number}</p>
-//                                 </div>
-//                             )}
-//                         </div>
-//                         <div className="modal-footer">
-//                             <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
+            // <div className="modal fade" id="userDetailsModal" tabIndex="-1" aria-labelledby="userDetailsModalLabel" aria-hidden="true">
+            //     <div className="modal-dialog">
+            //         <div className="modal-content">
+            //             <div className="modal-header">
+            //                 <h5 className="modal-title" id="userDetailsModalLabel">User Details</h5>
+            //                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            //             </div>
+            //             <div className="modal-body">
+            //                 {/* Display user details here */}
+            //                 {selectedUserDetails && (
+            //                     <div>
+            //                         <p style={{fontWeight:'bolder',fontSize:'1.2rem'}}><span style={{fontWeight:'bold'}}>Name: </span>{`${selectedUserDetails.first_name} ${selectedUserDetails.last_name}`}</p>
+            //                         <p style={{fontWeight:'bolder',fontSize:'1.2rem'}}><span style={{fontWeight:'bold'}}>Email:</span> {selectedUserDetails.email}</p>
+            //                         <p style={{fontWeight:'bolder',fontSize:'1.2rem'}}><span style={{fontWeight:'bold'}}>Enrollment Number:</span> {selectedUserDetails.enrollment_number}</p>
+            //                     </div>
+            //                 )}
+            //             </div>
+            //             <div className="modal-footer">
+            //                 <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            //             </div>
+            //         </div>
+            //     </div>
+            // </div>
 //             <div className="d-flex justify-content-center">
 //                 <button onClick={markAttendanceOfStudents} style={{ backgroundColor: '#c4779d', borderRadius: '50px', color: 'white', padding: '8px 20px', fontSize: '1.5rem', textAlign: 'center' }} type="button" className="btn mt-3">Mark</button>
 //             </div>
