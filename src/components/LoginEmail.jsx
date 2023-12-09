8847666a0aff28336b80a7d520acc2048b8a6402
ananya-2134 <ananyashrimali2334@gmail.com>
2023-12-09 16:27:07 +0530

    initial commit

--- src/components/LoginEmail.jsx
+++ src/components/LoginEmail.jsx
@@ -25,17 +25,17 @@ const Login = (props) => {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({email:email}),
+            body: JSON.stringify({ email: email }),
         });
         const res = await response.json();
-        if (res.results === true){
+        if (res.results === true) {
             updatedMail(email);
             navigate('/enterpassword');
-            SetAlert('Email Verified','success');
+            SetAlert('Email Verified', 'success');
         }
-        else if (res.results === false){
+        else if (res.results === false) {
             navigate('/register');
-            SetAlert('Please Register','primary');
+            SetAlert('Please Register', 'primary');
         }
         else {
             if (Array.isArray(res)) {
@@ -66,34 +66,96 @@ const Login = (props) => {
 
 
     return (
-        <div className="login-container">
-            <div className="pink-background">
-                <h1 className='attendance-text'><strong>ATTENDANCE</strong></h1>
-                <h1 className='made-text'><strong>MADE</strong></h1>
-                <h1 className='simple-text'><strong>SIMPLE.</strong></h1>
-            </div>
-            <div className="login-form">
-            <h1 className='welcome-text'>WELCOME TO ACROPORTAL</h1>
-            <div id="alert-container">
-            </div>
-                <h2 className='login-text' style={{ marginBottom: '50px' }}>Login</h2>
-                <form>
-                    <div>
-                        {/* <strong><label htmlFor="email">Email:</label></strong> */}
-                        <label className='email-text' htmlFor="email">Email:</label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            value={email}
-                            onChange={handleEmailChange}
-                            placeholder="Enter your email"
-                            className="rounded-input my-2"
-                        />
+        // <div className="login-container">
+        //     <section className="pink-background">
+        //         <h1 className='attendance-text'><strong>ATTENDANCE</strong></h1>
+        //         <h1 className='made-text'><strong>MADE</strong></h1>
+        //         <h1 className='simple-text'><strong>SIMPLE.</strong></h1>
+        //     </section>
+        //     <section className="login-form">
+        //         <div className='auth-content'>
+        //             <div id="alert-container"></div>
+        //             <h2 className='login-text' style={{ font: "bold 24px/29px Mona Sans, Helvetica Neue"}}>Sign in to Dribbble</h2>
+        //             <div className="auth-connections">
+        //                 <form className='auth-google-form '></form>
+        //             </div>
+        //             <hr className='divider sign-in' />
+        //             <div className='auth-form sign-in-form'>
+        //                 <form>
+        //                     <div>
+        //                         <label className='email-text' htmlFor="email">Email:</label>
+        //                         <input
+        //                             type="email"
+        //                             id="email"
+        //                             name="email"
+        //                             value={email}
+        //                             onChange={handleEmailChange}
+        //                             placeholder="Enter your email"
+        //                             className="rounded-input my-2"
+        //                         />
+        //                     </div>
+        //                     <button onClick={handleOnSubmit} className='continue-button' type="submit"><strong>Continue</strong></button>
+        //                 </form>
+        //             </div>
+        //         </div>
+        //     </section>
+        // </div>
+        <div id="main-container">
+            <section className="auth-sidebar">
+                <div className="auth-sidebar-content">
+                    <a href="/" className="auth-sidebar-logo">
+                        {/* Your SVG code here */}
+                    </a>
+                    <video
+                        playsInline
+                        className="auth-sidebar-video"
+                        autoPlay
+                        loop
+                        muted
+                        src="https://cdn.dribbble.com/uploads/48226/original/b8bd4e4273cceae2889d9d259b04f732.mp4?1689028949"
+                    ></video>
+                    <a className="auth-sidebar-credit" href="https://dribbble.com/shots/21785427-Mushroom-world-for-XR">
+                        @glebich
+                    </a>
+                </div>
+            </section>
+            <section className="content" data-content-container="">
+                <main>
+                    <div className="auth-content">
+                        <h2>Login to Acroportal</h2>
+                        <div className="auth-connections">
+                            {/* Google Sign-In Button */}
+                            <div
+                                id="g_id_onload"
+                                data-client_id="32073492121-s6ur8ti01mh34gq2bpbufb8ujdfrpn4v.apps.googleusercontent.com"
+                                data-login_uri="https://dribbble.com/auth/google_one_tap/callback"
+                                data-ux_mode="redirect"
+                            ></div>
+                            <form className="auth-google-form" action="/auth/google" acceptCharset="UTF-8" method="post">
+                                {/* Google Sign-In button code */}
+                            </form>
+                        </div>
+                        {/* <hr className="divider sign-in" /> */}
+                        <div className="auth-form sign-in-form">
+                            <form action="/session" acceptCharset="UTF-8" method="post">
+                                <label htmlFor="email">Email</label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    name="email"
+                                    value={email}
+                                    onChange={handleEmailChange}
+                                    placeholder="Enter your email"
+                                    className="rounded-input my-2"
+                                />
+                            </form>
+                            {/* <p className="auth-link color-deep-blue-sea-light-20">
+                                Don't have an account? <a className="underline" href="/signup/new">Sign up</a>
+                            </p> */}
+                        </div>
                     </div>
-                    <button onClick={handleOnSubmit} className='continue-button' type="submit"><strong>Continue</strong></button>
-                </form>
-            </div>
+                </main>
+            </section>
         </div>
     );
 };
